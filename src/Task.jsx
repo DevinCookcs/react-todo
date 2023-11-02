5d04ab7082606f19568260563d2f31f8b0dfeb1b
Devin Cook <144957285+DevinCookcs@users.noreply.github.com>
2023-11-02 17:47:04 -0600

    minor css change to complete button added id to button

--- src/Task.jsx
+++ src/Task.jsx
@@ -4,7 +4,7 @@ export const Task = (props) => {
             <h1 style={{textDecoration: props.completed ? "line-through" : "none"}}>
                 {props.taskName}
             </h1>
-            <button onClick={() => props.completeTask(props.id)}> Complete </button>
+            <button id="complete" onClick={() => props.completeTask(props.id)}> Complete </button>
             <button onClick={() => props.deleteTask(props.id)}> X </button>
         </div>
     )
