f1e80cf09bce68d6767ba64eddac335fb68f4a23
Devin Cook <144957285+DevinCookcs@users.noreply.github.com>
2023-11-03 14:13:55 -0600

    added id text-item and styling

--- src/Task.jsx
+++ src/Task.jsx
@@ -1,7 +1,7 @@
 export const Task = (props) => {
     return (
         <div className='task'>
-            <h1 style={{textDecoration: props.completed ? "line-through" : "none"}}>
+            <h1 id="task-item" style={{textDecoration: props.completed ? "line-through" : "none"}}>
                 {props.taskName}
             </h1>
             <button id="complete" onClick={() => props.completeTask(props.id)}> Complete </button>
